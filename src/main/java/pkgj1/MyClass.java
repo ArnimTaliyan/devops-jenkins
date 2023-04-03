51ccf5be8211920105f93cc1a87d714931492ece
ArnimTaliyan <arnimchaudhary@gmail.com>
2023-04-03 23:29:04 +0530

    test

--- src/main/java/pkgj1/MyClass.java
+++ src/main/java/pkgj1/MyClass.java
@@ -4,6 +4,9 @@ public class MyClass {
 	public void show() {
 		System.out.println("Hello DevOps! Welcome to Jenkins!");
 	}
+	public void display( ) {
+		System.out.println("Hello World!");
+	}
 	public static void main(String[] args) {
 		MyClass ob = new MyClass();
 		ob.show();
