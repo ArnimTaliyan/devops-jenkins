0960d35679201043d1638b8918a8c2c2f817c19a
ArnimTaliyan <arnimchaudhary@gmail.com>
2023-04-03 23:35:07 +0530

    new

--- src/main/java/pkgj1/MyClass.java
+++ src/main/java/pkgj1/MyClass.java
@@ -7,6 +7,9 @@ public class MyClass {
 	public void display( ) {
 		System.out.println("Hello World!");
 	}
+	public void f3( ) {
+		System.out.println("F3!");
+	}
 	public static void main(String[] args) {
 		MyClass ob = new MyClass();
 		ob.show();
